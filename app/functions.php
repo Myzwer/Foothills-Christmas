8e444c0e3199aa37baad265cdda8e001d0c82f0f
Josh Forrester <twicearoundmars@yahoo.com>
2019-11-08 19:26:07 -0500

    Build out the basics of the page - Header, info, saturday service, serve, faq and footer

--- app/functions.php
+++ app/functions.php
@@ -6,49 +6,65 @@ wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 // Foundation CSS
 function load_foundation() {
-	wp_enqueue_style( 'foundation.min', get_template_directory_uri() . '/assets/css/foundation.min.css');
+    wp_enqueue_style( 'foundation.min', get_template_directory_uri() . '/assets/css/foundation.min.css');
 }
 add_action('wp_enqueue_scripts', 'load_foundation');
 
 //Main stylesheet for general code
 function load_main_style() {
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css');
+    wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css');
 }
 add_action('wp_enqueue_scripts', 'load_main_style');
 
 
 //Font-Awesome Icons
 function load_font_awesome() {
-wp_register_style( 'Font_Awesome', 'https://use.fontawesome.com/releases/v5.7.1/css/all.css' );
-wp_enqueue_style('Font_Awesome');
+    wp_register_style( 'Font_Awesome', 'https://use.fontawesome.com/releases/v5.7.1/css/all.css' );
+    wp_enqueue_style('Font_Awesome');
 }
 add_action('wp_enqueue_scripts', 'load_font_awesome');
 
 
 
 //Foudnation JS
-		
+
 wp_enqueue_script( 'jquery' );
 
 function foundation_js_loadin() {
-wp_enqueue_script( 'foundation.min', get_template_directory_uri() . '/assets/js/vendor/foundation.min.js');
+    wp_enqueue_script( 'foundation.min', get_template_directory_uri() . '/assets/js/vendor/foundation.min.js');
 }
 add_action('wp_enqueue_scripts', 'foundation_js_loadin');
 
 function what_input_loadin() {
-wp_enqueue_script( 'what-input', get_template_directory_uri() . '/assets/js/vendor/what-input.js');
+    wp_enqueue_script( 'what-input', get_template_directory_uri() . '/assets/js/vendor/what-input.js');
 }
 add_action('wp_enqueue_scripts', 'what_input_loadin');
 
 
-//Foundation Icons
+//custom loadin
+function custom_js_loadin() {
+    wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/custom.js');
+}
+add_action('wp_enqueue_scripts', 'custom_js_loadin');
+
+
+//Font-Awesome Icons
 function icon_css() {
-wp_enqueue_style( 'foundation-icons', get_template_directory_uri() . '/assets/fonts/foundation-icons.css');
+    wp_enqueue_style( 'fontawesome.min', get_template_directory_uri() . '/assets/fonts/fontawesome.min.css');
 }
 add_action('wp_enqueue_scripts', 'icon_css');
 
-//Raleway Registration
-wp_register_style( 'raleway_font', 'https://fonts.googleapis.com/css?family=Raleway:400,700' );
-wp_enqueue_style('raleway_font');
+//Font-Awesome Icons
+function fc_icon_css() {
+    wp_enqueue_style( 'fc_icon', get_template_directory_uri() . '/assets/fonts/fc_icon.css');
+}
+add_action('wp_enqueue_scripts', 'fc_icon_css');
+
+
+//Gotham Font
+function gotham_font() {
+    wp_enqueue_style( 'gothamfont', get_template_directory_uri() . '/assets/fonts/gothamfont.css' );
+}
+add_action( 'wp_enqueue_scripts', 'gotham_font' );
 
 ?>
--- app/index.php
+++ app/index.php
@@ -16,6 +16,176 @@
  */
 
 get_header(); ?>
+    <video src="http://foothills-christmas.local/wp-content/uploads/2019/11/christmas-3.mp4" autoplay loop playsinline muted></video>
+    <header class="viewport-header">
+        <div class="grid-container">
+            <div class="grid-x grid-margin-x">
+                <div class="cell small-12 center">
+                    <img src="http://foothills-christmas.local/wp-content/uploads/2019/11/logo-1.png" alt="">
+                </div>
+                <div class="cell small-12 center">
+                    <a href="https://foothillschurch.churchcenter.com/login?return=https%3A%2F%2Ffoothillschurch.churchcenter.com%2Fregistrations%2Fevents%2F327898%2Fregistrations%2Fnew   " class="button">RSVP For Christmas!</a>
+                </div>
+            </div>
+        </div>
+    </header>
 
+    <main>
+        <div class="lower">
+            <div class="grid-container">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-12 medium-8 center">
+                        <div class="grid-x grid-margin-x">
+                            <div class="cell small-12">
+                                <h2 class = "header-details">What you need to know</h2>
+                                <p class="christmas-copy">We can’t wait to host A Foothills Christmas with you, your family and our community as we celebrate the birth of our savior Jesus. So, join us for one of four worship experiences at Foothills Church and take a moment to RSVP today! </p>
+                            </div>
+                            <div class="cell small-12">
+                                <h2 class = "header-details">Directions</h2>
+                                <span class = "service">Foothills Church</span>
+                                <span class="time-info">1551 W. Lamar Alexander Pkwy.</span>
+                                <span class="time-info">Maryville, TN 37801</span>
+                                <a class="directions-info" href = "https://goo.gl/maps/GtDR2QA4TSWXb7EXA">Directions</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="cell small-12 medium-4">
+                                <h2 class = "header-details">Dates and Times</h2>
+                                <span class = "service">1st Service</span>
+                                <span class="time-info">Saturday, December 21</span>
+                                <span class="time-info">6:00 p.m.</span>
+
+                                <span class = "service">2nd Service</span>
+                                <span class="time-info">Sunday, December 22</span>
+                                <span class="time-info">8:30 a.m.</span>
+
+                                <span class = "service">3rd Service</span>
+                                <span class="time-info">Sunday, December 22</span>
+                                <span class="time-info">10:00 a.m.</span>
+
+                                <span class = "service">4th Service</span>
+                                <span class="time-info">Sunday, December 22</span>
+                                <span class="time-info">11:30 a.m.</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="full-bg">
+            <div class="grid-container fullWidth">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-12 center">
+                        <h1 class = "neon upper-padding">Saturday Service</h1>
+                    </div>
+                    <div class="cell small-12 center">
+                        <p class="saturday-service">A Foothills Christmas begins Saturday, December 21 at 6 p.m. This evening worship experience will feature the same message and worship as our Sunday experience, as well as infant childcare. However, we will not include our regular FC Kids K-5 programming. We encourage all FC volunteers serving on Sunday to take advantage of this worship experience with their families. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+        <div class = "break"></div>
+
+    <main>
+        <div class="full-bg">
+            <div class="grid-container fullWidth">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-12 center">
+                        <h1 class = "neon padding-push">Serve at Christmas</h1>
+                    </div>
+                    <div class="cell small-12 center">
+                        <p class="saturday-service">We are looking for volunteers to serve in a variety of positions as we expect many first-time guests, family members and more to join us for A Foothills Christmas. If you would like to serve with our team, please let us know by completing the form below! </p>
+                    </div>
+                    <div class="cell small-12 center">
+                        <a href="https://foothillschurch.churchcenter.com/people/forms/5833" class="button">Serve at Christmas!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <div class = "break"></div>
+
+    <main>
+        <div class="full-bg">
+            <div class="grid-container fullWidth">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-12 center">
+                        <h1 class = "neon padding-push">FAQ</h1>
+                    </div>
+                    <div class="cell small-12 center">
+                        <div class="question neon">
+                            When is A Foothills Christmas?
+                        </div>
+                        <div class="answer">
+                            A Foothills Christmas will take place at Foothills Church over the course of four worship experiences beginning Saturday, December 21 at 6 p.m., and continuing Sunday, December 22 at 8:30 a.m., 10 a.m. and 11:30 a.m. There will be no services on Christmas Eve or Christmas Day.
+                        </div>
+                    </div>
+
+                    <div class="cell small-12 center">
+                        <div class="question neon">
+                            What Childcare options are available?
+                        </div>
+                        <div class="answer">
+                            The Saturday worship experience at A Foothills Christmas will include infant care, but we will not have regular K-5th Grade FC Kids Programming. All three Sunday worship experience will include infant care and regular K-5th Grade FC Kids programming.
+                        </div>
+                    </div>
+
+                    <div class="cell small-12 center">
+                        <div class="question neon">
+                            Will all four worship experiences be the same?
+                        </div>
+                        <div class="answer">
+                            Yes, all for worship experiences at A Foothills Christmas will include the same message and worship music. We encourage you to attend the service that best fits your family’s schedule.
+                        </div>
+                    </div>
+
+                    <div class="cell small-12 center">
+                        <div class="question neon">
+                            How can I volunteer?
+                        </div>
+                        <div class="answer">
+                            If you are interested in serving with one of our volunteer teams, please complete this form and a member of our staff will be in touch with opportunities across our ministries.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="grid-container footer">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-3 medium-offset-2 medium-2 center">
+                        <a href="https://www.facebook.com/foothillscollective/">
+                            <i class="fab fa-facebook"></i>
+                        </a>
+                    </div>
+                    <div class="cell small-3 medium-2 center">
+                        <a href="https://www.instagram.com/foothills.collective/">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                    <div class="cell small-3 medium-2 center">
+                        <a href="https://www.youtube.com/channel/UCX_IiZ45InnY5o85cB1i-BA/featured">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </div>
+
+                    <div class="cell small-3 medium-2 center">
+                        <a href="https://foothillschurch.com/">
+                            <img class = "icon" src="https://foothills-collective.com/wp-content/uploads/2019/11/fc_web_icon.png" width="40" "10" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="cell small-12 center">
+                    <div class="copyright">&copy; <?php echo date("Y"); ?> Foothills Church</div>
+                </div>
+            </div>
+
+        </div>
+    </main>
 
 <?php get_footer();
